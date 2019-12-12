88940d668d17628c9fb7ee4f517f67bd67ac4f52
James Barker <jimb@jum.local>
2019-12-11 19:14:00 -0500

    second

---
+++ script.py
@@ -0,0 +1,30 @@
+# base module imports
+import math
+import os
+import sys
+
+# third party imports
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('world'))
+print(greet('Jim'))
+
+r = requests.get('http://www.brattleborofoodcoop.coop')
+print(r.status_code)
+
+if r.status_code == 200:
+    print('success!')
+elif r.status_code == 404:
+    print('not found :(')
+
+name = input("Your name?")
+print("hello,", name)
